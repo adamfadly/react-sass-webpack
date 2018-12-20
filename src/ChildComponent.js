9fecc60fca083b8267dd6c06018a1442aab30468
Adam <adam@kitabisa.com>
2018-12-20 10:07:59 +0700

    second commit

---
+++ src/ChildComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Childcomponent.css";
+
+class ChildComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Red Header</h2>
+      </div>
+    );
+  }
+}
+
+export default ChildComponent;
